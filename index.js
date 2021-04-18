d72822ef57f4aa6e161716621f171ddceaf665b7
Christopher Enea <cenea@ltu.edu>
2021-04-18 15:01:17 -0400

    socket.io example

---
+++ index.js
@@ -0,0 +1,25 @@
+const { json } = require('express');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.emit('welcome message', 'Welcome to my test site');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('status', (status) => {
+        console.log(status);
+        socket.emit('chat message', 'typing...' + status);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ server.js
@@ -0,0 +1,19 @@
+const Express = require('express');
+const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = Express();
+
+
+(async () => {
+    await Mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    app.listen(process.env.PORT);
+  })();
